3febe3067096e9c6358b7ac04405ea2f3ead4534
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2024-10-27 12:08:17 +0100

    rename fork

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """Setup script for the PyMeta distribution."""
 from distutils.core import setup
 setup(
-    name="PyMeta3",
+    name="PyMeta3-wheel",
     version="0.5.1",
     url="https://github.com/wbond/pymeta3",
     description="Pattern-matching language based on OMeta for Python 3 and 2",
